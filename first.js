85d089aece92f71493ef967f9b6a2dbd3a7ca071
Sarthak Haldar <sarthakhaldarofficial@gmail.com>
2024-08-06 10:44:30 -0700

    Add files via upload

---
+++ first.js
@@ -0,0 +1,75 @@
+// // ios -> swift, obj C
+// // android -> java, kotlin
+// // facebook app, insta app, pinterest app, uber eats are made of react native
+// // Code Runner app extension
+// // node download: https://nodejs.org/en 
+// // node -v to check version
+// // sudo npm i expo-cli
+// // We can run react native project with Expo CLI
+// // React Native Tools Microsoft app extension
+// // React-Native/React/Redux snippets EQuimper
+// // Material Icon Theme
+// // sudo npx create-expo-app --template
+// // sudo npx expo start
+// // if the above command doesn't work then npm start
+// // command + D brings developer menu in simulator
+
+
+
+// // print statement
+// console.log("Hello World")
+
+
+// // addition
+// console.log( 2 + 2)
+
+// // variable
+// num = 2 + 2
+// console.log(num + 2)
+
+// // string variable
+// let user = 'Sarthak'
+// console.log(user)
+// console.log(typeof user)
+
+
+// // constants
+// let radius = 5
+// const pi = 3.14
+// area = pi * radius * radius
+// console.log(area)
+
+// Primitive data type
+// -> Number
+// -> String
+// -> Boolean
+// -> Null
+// -> Undefined
+// -> Symbol
+
+// // float
+// let flt = 7.5
+// console.log(typeof flt)
+// // o/p number
+
+
+// // float
+// let flt = 7/0
+// console.log(flt)
+// // o/p Infinity
+
+// // Joining two strings
+// let firstname = "Sarthak"
+// let lastname = "Haldar"
+// let user = firstname + " " + lastname
+// console.log(user);
+
+// // boolean
+// let bool = 5 < 6
+// console.log(bool);
+// console.log(typeof bool);
+// // o/p true
+// // o/p boolean
+
+
+
