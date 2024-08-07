2d27af2f31afd11c6fb4978c33985064c722e251
Sarthak Haldar <sarthakhaldarofficial@gmail.com>
2024-08-07 14:00:53 -0700

    Add files via upload

---
+++ App Flexdirection.js
@@ -0,0 +1,37 @@
+//Flexdirection
+
+import React from "react";
+import { View } from 'react-native';
+
+export default function App() {
+
+  return (
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row",
+      }}
+    >
+      <View style={{
+        backgroundColor: "dodgerblue",
+        width: 100,
+        height: 100
+      }} />
+
+      <View style={{
+        backgroundColor: "gold",
+        width: 100,
+        height: 100
+      }} />
+
+      <View style={{
+        backgroundColor: "tomato",
+        width: 100,
+        height: 100
+      }} />
+
+
+    </View >
+  );
+}
